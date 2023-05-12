0f9b9b96b6b4acfa20a64db10704f83a1dc8cb82
Maxim <bekrinmks@gmail.com>
2023-05-12 04:09:29 +0300

    layout bek4`s part without hover

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-new Swiper('');
+new Swiper('.image-slider', {
+
+    navigation: {
+        nextEl: '.swiper-button-next-modern',
+        prevEl: '.swiper-button-prev-modern'
+    },
+
+    pagination: {
+        el: '.swiper-paginations',
+        //буллеты
+        clickable: true
+    },
+});
